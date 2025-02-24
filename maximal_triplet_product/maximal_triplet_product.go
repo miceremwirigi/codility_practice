ae000ecc3430b4f497b1f37665362878da2ecba7
miceremwirigi <miceremwirigi@gmail.com>
2025-02-24 19:30:05 +0300

    adjusted maximal_triplet_product

--- maximal_triplet_product/maximal_triplet_product.go
+++ maximal_triplet_product/maximal_triplet_product.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"slices"
+)
+
 func main() {
 	// A := []int{4, 5, 1, 0}
 	A := []int{-3, 1, 2, -2, 5, 6}
@@ -8,17 +13,29 @@ func main() {
 
 func solution(A []int) int {
 	maximalProduct := 0
-	arrangedArray := make([]int, len(A))
-	copy(arrangedArray, A)
-	// Pick only consequtive ascending numbers
-	for i := 1; i < len(arrangedArray); i++ {
-		if arrangedArray[i] <= arrangedArray[i-1] {
-			arrangedArray = append(arrangedArray[:i], arrangedArray[i+1:]...)
-			i-=1
-		}
-	}
-	for i := 0; i < len(arrangedArray)-2; i++ {
-		maximalProduct = max(maximalProduct, arrangedArray[i]*arrangedArray[i+1]*arrangedArray[i+2])
+	B := make([]int, len(A))
+	copy(B, A)
+	// Sort the array
+	slices.Sort(B)
+	fmt.Println(A)
+	fmt.Println(B)
+
+	// arrangedArray := make([]int, len(A))
+	// copy(arrangedArray, A)
+	// // Pick only consequtive ascending numbers
+	// for i := 1; i < len(arrangedArray); i++ {
+	// 	if arrangedArray[i] <= arrangedArray[i-1] {
+	// 		arrangedArray = append(arrangedArray[:i], arrangedArray[i+1:]...)
+	// 		i-=1
+	// 	}
+	// }
+	// for i := 0; i < len(arrangedArray)-2; i++ {
+	// 	maximalProduct = max(maximalProduct, arrangedArray[i]*arrangedArray[i+1]*arrangedArray[i+2])
+	// }
+
+	// Check if there is a triangular triplet
+	for i := 0; i < len(B)-2; i++ {
+		maximalProduct = max(maximalProduct, B[i]*B[i+1]*B[i+2])
 	}
 
 	return maximalProduct
